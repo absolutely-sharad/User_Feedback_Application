647711042b0a757f42a0b2be03893bb834db7e96
Arihant-Singh-Rana <arihantsinghranafst@gmail.com>
2024-03-07 20:32:06 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import FeedBackApp from "./FeedBackApp";
+import "./App.css";
+export default function App() {
+  return (
+    <div className="App">
+      <FeedBackApp />
+    </div>
+  );
+}
